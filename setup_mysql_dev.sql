0955c2fabdb88becd407253b95752a6d52eef051
root <leadconsultnig@gmail.com>
2023-01-06 17:20:33 -0800

    ALL-combined_07_01_23

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,47 +1,33 @@
-import mysql.connector
+onnect to the MySQL server as the root user
 
-
-
--- Connect to the MySQL server as the root user
-
-conn = mysql.connector.connect(user='root', password='your_root_password', host='localhost')
-
-cursor = conn.cursor()
+import os
 
 
 
 -- Create the hbnb_dev_db database
 
-cursor.execute('CREATE DATABASE IF NOT EXISTS hbnb_dev_db')
+os.system("mysql -u root -pyour_root_password -e 'CREATE DATABASE IF NOT EXISTS hbnb_dev_db'")
 
 
 
 -- Create the hbnb_dev user
 
-cursor.execute("CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'")
+os.system("mysql -u root -pyour_root_password -e \"CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'\"")
 
 
 
 -- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
 
-cursor.execute("GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'")
+os.system("mysql -u root -pyour_root_password -e \"GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'\"")
+
+os.system("mysql -u root -pyour_root_password -e 'FLUSH PRIVILEGES'")
 
 
 
 -- Grant SELECT privilege on the performance_schema database to the hbnb_dev user
 
-cursor.execute("GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'")
+os.system("mysql -u root -pyour_root_password -e \"GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'\"")
 
-
-
--- Commit the changes
-
-conn.commit()
-
-
-
--- Close the connection
-
-conn.close()
+os.system("mysql -u root -pyour_root_password -e 'FLUSH PRIVILEGES'")
 
 
