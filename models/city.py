475fed3a0da26a8765e2d4e606b6edf0a7d0e6ac
root <leadconsultnig@gmail.com>
2023-01-06 18:24:07 -0800

    ALL-combined_07_01_23

--- models/city.py
+++ models/city.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python3
-"""Module for City class."""
+class State(BaseModel, Base):
+    """ The state class, contains a name and a list of cities """
+    # ...
 
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    """Class representing a City."""
-    state_id = ""
-    name = ""
-
+    @property
+    def cities(self):
+        from models.city import City
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
