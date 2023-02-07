4a62656abfd5944a7231477fdcec840755c82b28
LeadConsult <106965102+LeadConsult@users.noreply.github.com>
2023-02-07 01:18:27 +0100

    Update 100-hbnb.py

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 """
-Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /hbnb: HBnB home page.
+Starts a Flask web application
 """
 from models import storage
 from flask import Flask
@@ -13,21 +9,24 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Displays the main HBnB filters HTML page."""
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """
+    Displays the main HBnB filters HTML page.
+    """
     states = storage.all("State")
     amenities = storage.all("Amenity")
-    places = storage.all("Place")
-    return render_template("100-hbnb.html",
-                           states=states, amenities=amenities, places=places)
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """
+    Remove the current SQLAlchemy session.
+    """
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
